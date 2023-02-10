0b680c54bbea0f1b1857015ae74784663438b616
adrian <adrian.basson@rysis.co.za>
2023-02-10 09:44:08 +0200

    Added api calls.

---
+++ CoinJar/Controllers/CoinJar.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoinJar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoinJar : Controller
+    {
+        [Route("GetTotalAmount")]
+        [HttpGet]
+        public IActionResult GetTotalAmount()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Route("AddCoin")]
+        [HttpPost]
+        public IActionResult AddCoin()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Route("ResetCoins")]
+        public IActionResult ResetCoins()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
