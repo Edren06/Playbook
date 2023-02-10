41005ad7a9582678b9b53236b71ff29a7a2ed579
adrian <adrian.basson@rysis.co.za>
2023-02-10 14:27:36 +0200

    Stadium controller updated.

--- RugbyManagement/Controllers/StadiumController.cs
+++ RugbyManagement/Controllers/StadiumController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RugbyManagement.Framework.ViewModels;
+using System.Numerics;
 
 namespace RugbyManagement.Controllers
 {
@@ -8,23 +10,49 @@ namespace RugbyManagement.Controllers
     {
         [Route("GetStadium")]
         [HttpGet]
-        public IActionResult GetStadium(int? stadiumId)
+        public IActionResult GetStadium(int stadiumId)
         {
-            return View();
+            try
+            {
+                StadiumVm vm = new StadiumVm();
+                return new JsonResult(vm.GetStadium(stadiumId));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [Route("ListStadiums")]
         [HttpGet]
-        public IActionResult UpsertStadium()
+        public IActionResult ListStadiums()
         {
-            return View();
+            try
+            {
+                StadiumVm vm = new StadiumVm();
+                return new JsonResult(vm.GetStadiumList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [Route("UpsertStadium")]
         [HttpPost]
-        public IActionResult UpsertStadium(ListStadiumsResult stadium)
+        public IActionResult UpsertStadium(GetStadiumResult stadium)
         {
-            return View();
+            try
+            {
+                StadiumVm vm = new StadiumVm();
+                vm.UpsertStadium(stadium);
+
+                return new JsonResult(stadium.StadiumId == 0 ? "Stadium created" : "Stadium Upserted");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
